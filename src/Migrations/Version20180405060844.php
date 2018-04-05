40c8d4bc69202babe6985057383542ba994d13b8
Воронова Ольга <lady.voronova.olga@gmail.com>
2018-04-05 09:58:56 +0300

    Тестовые пользователи (у всех пароль 123)

--- src/Migrations/Version20180405060844.php
+++ src/Migrations/Version20180405060844.php
@@ -19,6 +19,11 @@ class Version20180405060844 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('ALTER TABLE user CHANGE phone phone VARCHAR(20) DEFAULT NULL');
+        $this->addSql('INSERT INTO `user` (`id`, `username`, `password`, `email`, `role`, `is_active`, `name`, `surname`, `patronymic`, `reg_date`, `phone`) VALUES
+(1,	\'admin\',	\'$2y$13$pi4uqVufIKj3U/88tzAc9eBVs/UqXOLMxvQwUQF32vrUsRYb1yOJq\',	\'admin@admin.ru\',	\'[\"ROLE_SUPER_ADMIN\"]\',	1,	\'admin\',	\'admin\',	\'admin\',	1522908555,	\'+7 (925) 865 8084\'),
+(2,	\'user\',	\'$2y$13$UCffwzP3/WduCOArWfJLV.dv5TmJFIW2vucIps9M1n/.ADTFvk81.\',	\'user@user.ru\',	\'[\"ROLE_USER\"]\',	1,	\'user\',	\'user\',	\'user\',	1522911125,	\'+7 (925) 289 6649\'),
+(3,	\'moderator\',	\'$2y$13$06nPEjAWnlG/QHLPaMEAWOedfuFPpUQx6r3uaEYRC4CKOVLDsFA1K\',	\'moderator@moderator.ru\',	\'[\"ROLE_ADMINISTRATOR\"]\',	1,	\'moderator\',	\'maderator\',	\'moderator\',	1522911237,	\'+7 (916) 123 4567\'),
+(4,	\'author\',	\'$2y$13$JTSfoEJqMroOgIqsH6kxYuysKBqrDPNuvEXwn0uNByX30HaDrw5K2\',	\'author@author.ru\',	\'[\"ROLE_MASTER\"]\',	1,	\'author\',	\'author\',	\'author\',	1522911363,	\'+7 (123) 456 7899\')');
     }
 
     /**
