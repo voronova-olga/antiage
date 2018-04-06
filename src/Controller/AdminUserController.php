c6bad3024e724e2557c057e369482c97d65af11d
Воронова Ольга <lady.voronova.olga@gmail.com>
2018-04-06 09:39:58 +0300

    Работа с пользователем, макрос склонения существительных

--- src/Controller/AdminUserController.php
+++ src/Controller/AdminUserController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
@@ -23,7 +24,7 @@ class AdminUserController extends Controller
     /**
      * @Route("/admin/user/add", name="admin_user_add")
      */
-    public function add()
+    public function add(EntityManagerInterface $em)
     {
         return $this->render('admin_user/add.html.twig', [
             'controller_name' => 'AdminUserController',
@@ -33,10 +34,15 @@ class AdminUserController extends Controller
     /**
      * @Route("/admin/user/edit/{id}", name="admin_user_edit")
      */
-    public function edit($id)
+    public function edit($id, EntityManagerInterface $em)
     {
+        $repository = $this->getDoctrine()->getRepository(User::class);
+        $user = $repository->find($id);
+        if(!$user){
+            return $this->redirectToRoute('admin_user');
+        }
         return $this->render('admin_user/edit.html.twig', [
-            'controller_name' => 'AdminUserController',
+            'user' => $user,
         ]);
     }
 
@@ -45,24 +51,49 @@ class AdminUserController extends Controller
      */
     public function info($id)
     {
+        $repository = $this->getDoctrine()->getRepository(User::class);
+        $user = $repository->find($id);
+        if(!$user){
+            return $this->redirectToRoute('admin_user');
+        }
         return $this->render('admin_user/info.html.twig', [
-            'controller_name' => 'AdminUserController',
+            'user' => $user,
         ]);
     }
 
     /**
      * @Route("/admin/user/active/{id}", name="admin_user_active")
      */
-    public function active($id)
+    public function active($id, EntityManagerInterface $em)
     {
+        $repository = $this->getDoctrine()->getRepository(User::class);
+        $user = $repository->find($id);
+        if(!$user){
+            return $this->redirectToRoute('admin_user');
+        }
+        if($user->getIsActive()){
+            $user->setIsActive(FALSE);
+        }else{
+            $user->setIsActive(TRUE);
+        }
+        $em->persist($user);
+        $em->flush();
         return $this->redirectToRoute('admin_user');
     }
 
     /**
      * @Route("/admin/user/delete/{id}", name="admin_user_delete")
      */
-    public function delete($id)
+    public function delete($id, EntityManagerInterface $em)
     {
+        $repository = $this->getDoctrine()->getRepository(User::class);
+        $user = $repository->find($id);
+        if(!$user){
+            return $this->redirectToRoute('admin_user');
+        }
+        $user->setIsDelete(TRUE);
+        $em->persist($user);
+        $em->flush();
         return $this->redirectToRoute('admin_user');
     }
 }
